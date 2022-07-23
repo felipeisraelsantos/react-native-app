90b58e02b6127c19b2dee2bc8f328053a0824ccb
felipe.israel <felipe.israel@aluno.ifsp.edu.br>
2022-07-23 15:51:18 -0300

    INtegração com Firebase

--- rockethelp/src/screens/SignIn.tsx
+++ rockethelp/src/screens/SignIn.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import auth from '@react-native-firebase/auth';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 import { Envelope, Key } from 'phosphor-react-native';
 
@@ -6,16 +7,21 @@ import Logo from '../assets/logo_primary.svg';
 
 import { Input } from '../components/Input';
 import { Button } from '../components/Button';
+import { Alert } from 'react-native';
 
 export function SignIn() {
-  const [name, setName] = useState('João');
+  const [email, setEmail] = useState('João');
   const [password, setPassword] = useState('');
 
   const { colors } = useTheme();
 
-  // function handleSignIn() {
-  //   console.log(name, password);
-  // }
+  function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert("Entrar", "informe e-mail e senha");
+    }
+
+
+  }
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -29,7 +35,7 @@ export function SignIn() {
         mb={4}
         placeholder="E-mail"
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} ml={4} />}
-        onChangeText={setName}
+        onChangeText={setEmail}
       />
 
       <Input
