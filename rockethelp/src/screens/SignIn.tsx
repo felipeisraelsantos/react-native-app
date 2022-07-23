7ce94315bdcba09915c45616b13e185180daa3eb
felipe.israel <felipe.israel@aluno.ifsp.edu.br>
2022-07-23 16:20:22 -0300

    add gitignore

--- rockethelp/src/screens/SignIn.tsx
+++ rockethelp/src/screens/SignIn.tsx
@@ -10,6 +10,7 @@ import { Button } from '../components/Button';
 import { Alert } from 'react-native';
 
 export function SignIn() {
+  const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState('João');
   const [password, setPassword] = useState('');
 
@@ -19,8 +20,28 @@ export function SignIn() {
     if (!email || !password) {
       return Alert.alert("Entrar", "informe e-mail e senha");
     }
+    setIsLoading(true);
 
+    auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      console.log(error);
+      setIsLoading(false);
 
+      // if (error.code === 'auth/invalid-email') {
+      //   return Alert.alert('Entrar', 'E-mail inválido.');
+      // }
+
+      // if (error.code === 'auth/wrong-password') {
+      //   return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+      // }
+
+      // if (error.code === 'auth/user-not-found') {
+      //   return Alert.alert('Entrar', 'E-mail ou senha inválida.');
+      // }
+
+      // return Alert.alert('Entrar', 'Não foi possível acessar');
+    });
   }
 
   return (
@@ -46,8 +67,11 @@ export function SignIn() {
         onChangeText={setPassword}
       />
 
-      <Button title="Entrar" w="full" 
-      // onPress={handleSignIn} 
+      <Button 
+        title="Entrar" 
+        w="full" 
+        onPress={handleSignIn}
+        isLoading={isLoading} 
       />
     </VStack>
   )
